b3cbbc13c6e9a1392dd90c4ccedce94f05b3cc52
BrookeBryski <brookeabryski@gmail.com>
2023-08-04 12:51:35 -0700

    finished up password custom middleware

--- index.js
+++ index.js
@@ -10,13 +10,35 @@ app.use((req, res, next) => {
     next();
 })
 
+app.use('/dogs', (req, res, next) => {
+    console.log("I love dogs!")
+    next();
+})
+
+const verifyPassword = (req, res, next) => {
+    const { password } = req.query;
+    if (password === 'chickennugget'){
+        next();
+    }
+    res.send('Error: Invalid password')
+}
+
 app.get('/', (req, res) => {
     console.log(`REQUEST DATE: ${req.requestTime}`)
     res.send('home');
 })
 
 app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
     res.send('woof');
+})
+
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('I have no idea if I am an introvert or extrovert');
+})
+
+app.use((req, res) => {
+    res.status(404).send('Error: not found')
 })
 
 app.listen(3000, () => {
